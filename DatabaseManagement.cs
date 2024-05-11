5c6ae337096cf19d31c50b249a52486cf218aae5
x1stanzz <vova28032003@gmail.com>
2024-05-11 17:36:48 +0300

    Добавьте файлы проекта.

---
+++ DatabaseManagement.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_1
+{
+    internal class DatabaseManagement
+    {
+        private String connectionString;
+        public DatabaseManagement(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void DisplayTables()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection open");
+
+                    PrintTableData(connection, "Author");
+
+                    PrintTableData(connection, "Publisher");
+
+                    PrintTableData(connection, "Genre");
+
+                    PrintTableData(connection, "Customer");
+
+                    PrintTableData(connection, "Book");
+
+                    PrintTableData(connection, "Review");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    Console.WriteLine("Connection close");
+                }
+            }
+        }
+        private void PrintTableData(SqlConnection connection, string tableName)
+        {
+            string sql = $"SELECT * FROM {tableName}";
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            Console.WriteLine($"\nData from table {tableName}:\n");
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader.GetName(i)}: {reader.GetValue(i)}\t");
+                }
+                Console.WriteLine();
+            }
+            reader.Close();
+        }
+
+        public void PrintTableByName(string tableName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection open");
+
+                    PrintTableData(connection, tableName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    Console.WriteLine("Connection close");
+                }
+            }
+        }
+        public void AddDataToTable(string tableName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine($"Connection opened");
+
+                    int currentMaxId = GetCurrentMaxId(tableName);
+
+                    Console.Write($"Enter Id (current value: {currentMaxId}): ");
+                    int newId = int.Parse(Console.ReadLine());
+
+                    SqlCommand cmd = new SqlCommand($"SELECT * FROM {tableName}", connection);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    reader.Read();
+                    int columnCount = reader.FieldCount;
+                    string[] columnNames = new string[columnCount];
+                    for (int i = 0; i < columnCount; i++)
+                    {
+                        columnNames[i] = reader.GetName(i);
+                    }
+                    reader.Close();
+
+                    string[] values = new string[columnCount];
+                    for (int i = 0; i < columnCount; i++)
+                    {
+                        if (columnNames[i] == "Id")
+                        {
+                            values[i] = newId.ToString();
+                            continue;
+                        }
+
+                        Console.Write($"Enter value for column '{columnNames[i]}': ");
+                        string value = Console.ReadLine();
+                        values[i] = $"'{value}'";
+                    }
+
+                    string columns = string.Join(", ", columnNames);
+                    string columnValues = string.Join(", ", values);
+
+                    string sql = $"INSERT INTO {tableName} ({columns}) VALUES ({columnValues});"; 
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine("Data was successfully added.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    Console.WriteLine($"Connection closed.");
+                }
+            }
+        }
+
+        private int GetCurrentMaxId(string tableName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand($"SELECT MAX(Id) FROM {tableName}", connection);
+                object result = cmd.ExecuteScalar();
+                if (result == DBNull.Value)
+                {
+                    return 0; 
+                }
+                else
+                {
+                    return Convert.ToInt32(result);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace lab_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=X1STANZZ;Database=lab_2_1;Trusted_Connection=True;";
+
+            DatabaseManagement dbManagement = new DatabaseManagement(connectionString);
+
+            while (true)
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Show all tables");
+                Console.WriteLine("2. Show table data");
+                Console.WriteLine("3. Add data into table");
+                Console.WriteLine("4. Exit");
+                Console.Write("Choose option: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        dbManagement.DisplayTables();
+                        break;
+                    case "2":
+                        Console.Write("Choose table: ");
+                        string option = Console.ReadLine();
+                        dbManagement.PrintTableByName(option);
+                        break;
+                    case "3":
+                        Console.Write("Choose table: ");
+                        string option_addData = Console.ReadLine();
+                        dbManagement.AddDataToTable(option_addData);
+                        break;
+                    case "4":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Try again!");
+                        break;
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
